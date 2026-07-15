382f707065a2d2468da6526e1b8d7d9f3dc288c4
vahermihkel <65609256+vahermihkel@users.noreply.github.com>
2026-07-15 11:30:42 +0300

    Update ProductDetails.tsx

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -2,10 +2,10 @@
 // HomePage-s tootele vajutamine ja siia lehele sattumine
 // Siin lehel backendi päring ühe toote osas (ID kaudu toote võtmine)
 
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
 function ProductDetails() {
-    const {id} = useParams(); // <-- võtab URLi muutuja
+    // const {id} = useParams(); // <-- võtab URLi muutuja
 
   return (
     <div>ProductDetails</div>
